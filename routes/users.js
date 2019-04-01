8b2d60c340c23c65c8a069a80f54450ecbde993a
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-04-01 15:39:04 +0530

    complete user registration in api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 // Register
-router.get('/register', (req, res, next) => {
- res.send('Register');
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,  //req.body mean the value is post using text field or other
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {  //call the addUser function in User model
+    if(err){
+      res.json({success: false, msg:'Failed to register user'});
+    } else {
+      res.json({success: true, msg:'User registered'});
+    }
+  });
 });
 
 // Authenticate
@@ -14,11 +30,6 @@ router.post('/authenticate', (req, res, next) => {
 // Profile
 router.get('/profile', (req, res, next) => {
   res.send('PROFILE');
-});
-
-// Validate
-router.get('/validate', (req, res, next) => {
-  res.send('VALIDATE');
 });
 
 module.exports = router;
