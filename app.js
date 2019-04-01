1e01087e7e84d837fd6b255eae0a4507daa3a473
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-04-01 14:20:00 +0530

    connected to mongo db atlas

--- app.js
+++ app.js
@@ -5,16 +5,44 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+// DB config 
+const db = require('./config/keys').MongoURI;
+
+ 
+//connect to mongo
+
+mongoose.connect(db,{ useNewUrlParser:true})
+
+    .then(() => console.log('Connected to mongodb successfully............!'))
+
+    .catch(err => console.log(err));
+
+
 
 const app = express();
 
+const users = require('./routes/users');
+
+// Port Number
 const port = 3000;
 
-app.get('/',(req,res)=>{
-    res.send('Invalid Endpoint');
+// CORS Middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
 });
 
-
-app.listen(port, ()=>{
-    console.log('Server started on port '+port);
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port '+port);
 });
---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+ 
+    MongoURI: 'mongodb+srv://kush:kush123@cluster0-nvdih.mongodb.net/volunteering?retryWrites=true' //db name volunteeringrec
+     //Auto create the db using this string
+
+}
+
+ 
