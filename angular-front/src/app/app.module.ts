145d8537b571e95c1f2edbbfcc86e0bcd5d9d9cb
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-04-02 23:54:28 +0530

    connecting with api

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
 import {RouterModule, Routes} from '@angular/router';
 import { NgFlashMessagesModule } from 'ng-flash-messages';
