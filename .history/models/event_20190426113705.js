747c583d6ed3a77ef2dcdaabd5f3435cc1a6fc65
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-04-26 17:13:35 +0530

    create event model

---
+++ .history/models/event_20190426113705.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+ 
+
+// Schema
+const EventSchema = mongoose.Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const Event = module.exports = mongoose.model('Event', EventSchema);
+
+module.exports.getEventById = function(id, callback){
+  Event.findById(id, callback);
+}
+
+module.exports.getEventByEventname = function(eventname, callback){
+  const query = {eventname: eventname}
+  User.findOne(query, callback);
+}
+
+module.exports.addEvent = function(newEvent, callback){
+   
+      if(err) throw err;
+      newEven.save(callback);
+ 
+
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+  bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+    if(err) throw err;
+    callback(null, isMatch);
+  });
+}
