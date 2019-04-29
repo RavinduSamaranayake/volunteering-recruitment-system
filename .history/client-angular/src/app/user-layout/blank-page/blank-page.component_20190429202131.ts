11a1eabbbfdce9d2ec01101290108d26f702384e
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-04-29 21:07:07 +0530

    complete view user profile

---
+++ .history/client-angular/src/app/user-layout/blank-page/blank-page.component_20190429202131.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-blank-page',
+    templateUrl: './blank-page.component.html',
+    styleUrls: ['./blank-page.component.scss']
+})
+export class BlankPageComponent implements OnInit {
+    firstname: String;
+    lastname: String;
+    username: String;
+    email: String;
+    password: String;
+    conpassword: String;
+    address: String;
+    address2: String;
+    cntctmob: String;
+    cntctfix: String;
+    age: String;
+  
+    constructor(
+        private validateService: ValidateService,
+        private authService: AuthService,
+        private router: Router,
+        private alerts: AlertsService
+    ) {}
+  
+    ngOnInit() {
+    }
+  
+    onRegisterSubmit() {
+        const user = {
+          firstname: this.firstname,
+          lastname: this.lastname,
+          email: this.email,
+          username: this.username,
+          password: this.password,
+          rpassword: this.conpassword,
+          address: this.address,
+          address2: this.address2,
+          cntctmob: this.cntctmob,
+          cntctfix: this.cntctfix,
+          age: this.age,
+        };
+    
+  
+}
---
+++ .history/client-angular/src/app/user-layout/blank-page/blank-page.component_20190429203207.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../myservices/validate.service';
+import { AuthService } from '../../myservices/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-blank-page',
+    templateUrl: './blank-page.component.html',
+    styleUrls: ['./blank-page.component.scss']
+})
+export class BlankPageComponent implements OnInit {
+    firstname: String;
+    lastname: String;
+    username: String;
+    email: String;
+    password: String;
+    conpassword: String;
+    address: String;
+    address2: String;
+    cntctmob: String;
+    cntctfix: String;
+    age: String;
+
+    constructor(
+        private validateService: ValidateService,
+        private authService: AuthService,
+        private router: Router,
+    ) {}
+  
+    ngOnInit() {
+        this.
+    }
+  
+    onRegisterSubmit() {
+        const user = {
+          firstname: this.firstname,
+          lastname: this.lastname,
+          email: this.email,
+          username: this.username,
+          password: this.password,
+          rpassword: this.conpassword,
+          address: this.address,
+          address2: this.address2,
+          cntctmob: this.cntctmob,
+          cntctfix: this.cntctfix,
+          age: this.age,
+        };
+    }
+}
