a08f4759997076f0869bd05afe29ac6207e1e05f
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-05-25 15:46:30 +0530

    set the all counters to stat models

---
+++ .history/client-angular/src/app/myservices/organization.service_20190523113509.ts
@@ -0,0 +1,74 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrganizationService {
+  // this is use to share the selected event data pass to the viewdata component via event service
+  // $eventdetails = new EventEmitter(); // this is like value saving varible
+  // eventId: String;
+  // slctevent: any;
+
+  constructor(private http: HttpClient) {}
+
+  addOrganization(org) {
+    return this.http.post('http://localhost:3000/organizations/addorganization', org);
+  }
+
+  getAllOrganizations() {
+    // let headers = new HttpHeaders();
+    return this.http.get('http://localhost:3000/organizations/getallorganizations');
+  }
+
+  getOrganizationById(id: string) {
+    // let headers = new HttpHeaders();
+    return this.http.get('http://localhost:3000/organizations/getorganizationbyid/' + id);
+  }
+
+  getOrganizationEvents(id: string) {
+    // let headers = new HttpHeaders();
+    return this.http.get('http://localhost:3000/events/selecteventbyorg/' + id);
+  }
+
+  editAccessOrganization(org: any) {
+    return this.http.put('http://localhost:3000/organizations/editaccess/', org);
+  }
+
+  // // after the user select a event.......................
+
+  // setEvent(sevent) {
+  //   this.slctevent = sevent;
+  //   this.$eventdetails.emit(this.slctevent); // save the event value in this variable
+  // }
+
+  // addSelectEvent(event) {
+  //   let headers = new HttpHeaders();
+  //   headers.append("Content-Type", "application/json");
+  //   // post the user data to the server in json object
+  //   return this.http.post("http://localhost:3000/events/addselected", event, {
+  //     headers: headers
+  //   });
+  // }
+
+  // getAllSelectEvent(userid) {
+  //   // let headers = new HttpHeaders();
+  //   return this.http.get(
+  //     "http://localhost:3000/events/allselectevents/" + userid
+  //   );
+  // }
+
+  // getAllUpcommingEvent(userid) {
+  //   // let headers = new HttpHeaders();
+  //   return this.http.get(
+  //     "http://localhost:3000/events/allselect/upcomming/" + userid
+  //   );
+  // }
+
+  // getAllEventHistory(userid) {
+  //   // let headers = new HttpHeaders();
+  //   return this.http.get(
+  //     "http://localhost:3000/events/allselect/history/" + userid
+  //   );
+  // }
+}
---
+++ .history/client-angular/src/app/user-layout/userdashboard/userdashboard.component_20190525142006.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { routerTransition } from '../../router.animations';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-userdashboard',
+  templateUrl: './userdashboard.component.html',
+  styleUrls: ['./userdashboard.component.scss'],
+  animations: [routerTransition()]
+})
+export class UserdashboardComponent implements OnInit {
+  public alerts: Array<any> = [];
+    public sliders: Array<any> = [];
+
+    constructor(
+        private router: Router,
+
+        );
+
+
+    ngOnInit() {}
+
+    public closeAlert(alert: any) {
+        const index: number = this.alerts.indexOf(alert);
+        this.alerts.splice(index, 1);
+    }
+
+    public viewAvailable() {
+       console.log('.............clicked it.........');
+       this.router.navigate(['projects']);
+
+    }
+
+    public viewUpcomming() {
+        console.log('.............clicked UPCOMMING.........');
+        this.router.navigate(['upcomming']);
+     }
+
+     public viewHistory() {
+        console.log('.............clicked HISTORY.........');
+        this.router.navigate(['history']);
+     }
+     public viewOrganization() {
+        this.router.navigate(['organizations']);
+     }
+
+}
