420e4e14d3562917cb896eab1535df5b5e4fdec2
Chamith95 <chamithjay@gmail.com>
2019-06-05 00:19:55 +0530

    debugging

--- middleware/orgAuth.js
+++ middleware/orgAuth.js
@@ -3,8 +3,7 @@ const config = require("../config/keys");
 
 module.exports=(req,res,next)=>{
     const token2=req.headers.authorization.split(" ")[1];
-    console.log(token2);
-    console.log("dsdsa");
+
     try{
     const token=req.headers.authorization.split(" ")[1];
 
